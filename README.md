47b192c039cbf8946fd68c7aeaf6fc1c1d2c8340
Nick Jason Hagen <nickjasonhagen@gmail.com>
2025-10-04 15:28:51 +0200

    updated and readme

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # python_imgageresize
 Python command line tools to resize images
-syntax: -i (inputfile) -o (outputfile) -w (width) -h (height)
+## syntax
+- -i (inputfile)
+- -o (outputfile)
+- -w (width)
+- -h (height)
+```bash
+python resizeimg.py -i ./myimage.png -o ./mysizedimage.png -w 250 -h 250
+`
--- resizeimg.py
+++ resizeimg.py
@@ -9,23 +9,15 @@ def resize_image(image_path, output_path, width, height):
             img.save(output_path)
     except Exception as e:
         print(f"Error resizing image: {e}")
-
-def main():
-    parser = argparse.ArgumentParser(description='Resize an image file')
-    parser.add_argument('--i', help='Path to the image file')
-    parser.add_argument('--o', help='Path to the image file')
-    parser.add_argument('--w', type=int, default=800, help='New width of the resized image')
-    parser.add_argument('--h', type=int, default=600, help='New height of the resized image')
-
-    args = parser.parse_args()
-
-    if not os.path.exists(args.i):
-        print(f"Error: Image file '{args.image_path}' does not exist")
-        return
-
+parser = argparse.ArgumentParser(description='Resize an image file')
+parser.add_argument('--i', help='Path to the image file')
+parser.add_argument('--o', help='Path to the image file')
+parser.add_argument('--w', type=int, default=800, help='New width of the resized image')
+parser.add_argument('--h', type=int, default=600, help='New height of the resized image')
+args = parser.parse_args()
+if not os.path.exists(args.i):
+    print(f"Error: Image file '{args.image_path}' does not exist")
+else:
     output_path = f"{os.path.splitext(args.i)[0]}_{args.w}x{args.h}{os.path.splitext(args.o)[1]}"
     resize_image(args.i, args.o, args.w, args.h)
     print(f"Resized image saved to '{args.o}'")
-
-#if __name__ == "__main__":
-main()
